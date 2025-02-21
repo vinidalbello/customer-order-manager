057b194818b33b9660c33accad1253880da30a61
Vinicius Dal Bello <vinicius.cej@gmail.com>
2025-02-20 22:26:54 -0300

    ADD allowed host to railway url

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,7 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	server: {
+		host: true,
+	},
+	preview: {
+		allowedHosts: ['customer-order-manager-production.up.railway.app'],
+	},
 });
